86c7df99f8812ac116a6120dfacfa362fc6c886e
Iulian Catalin Mormeci <iulian.mormeci@proton.me>
2025-06-30 14:05:09 +0200

    creato file router con all interno una classe router, all interno del file e presente un attributo privato di tipo array vuoto che andra riempito con le varie rotte, all interno poi ci sono due metodi, uno add per l aggiunta delle rotte all interno del array vuoto di tipo associativo, e un altro metodo di tipo match che prende come parametro all interno il path e ne ritorna i parametri se all interno trova la rotta oppure ne ritorna un valore booleano false nel caso non trovi nulla. all interno del file index ho cambiato il modo in cui vengono chiamati il controller e la action usando l oggetto router, all interno della variabile params se il metodo match ritorna un array, si trovano il controller e la action da eseguire, quindi sono andato a passare all interno del controller e del action params con chiave controller e action. aggiunta di commenti sui vari file all interno del progetto

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 /* variabile che contiene l url della pagina, preso usando il metodo parse_url e all interno usando la funzione request_uri all interno della variabile _server associata all interno dell array alla voce request uri poi usando il metodo php_url_path */
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-/* uso della funzione explode per delimitare il path usando come separatore /  */
-$segments = explode("/", $path);
+/* importazione del file router */
+require "src/router.php";
+/* inizializazione di un nuovo oggetto router */
+$router = new Router;
+/* aggiunta delle rotte del sito web usando il metodo della classe router add() e passando come parametro all interno path */
+$router->add("/home/index", ["controller" => "home", "action" => "index"]); /* rotta per la home index */
+$router->add("/products", ["controller" => "products", "action" => "index"]);/* rotta per i products */
+$router->add("/", ["controller" => "home", "action" => "index"]);/* rotta per la home page */
+
+/* creazione variabile e esecuzione del metodo match con passaggio della variabile path all interno */
+$params = $router->match($path);
+/* controllo se all interno della variabile params e presente un valore booleano false per rotta non trovata, se trova false esce e ritorna il messaggio rotta non trovata */
+if($params === false)
+{
+    exit("No route to match");
+}
+
 /* passaggio dei pezi del path all interno del action e del controller */
-$action = $segments[2];
-$controller = $segments[1];
+$action = $params["action"];
+$controller = $params["controller"];
 /* importazione del controller in modo dinamico attraverso la variabile controller */
 require "src/controllers/$controller.php";
 /* nuovo oggetto creato usando la variabile controller di prima, che aiuta a creare una nuova istanza in modo dinamico del controller che ci serve */
---
+++ src/router.php
@@ -0,0 +1,30 @@
+<?php 
+/* classe router con all interno un attributo e due metodi */
+class Router
+{
+    /* attributo array vuoto che conterra le rotte */
+    private array $routes = [];
+/* metodo che aggiunge le rotte e che vuole come parametri una stringa path e i parametri */
+    public function add(string $path, array $params): void
+    {
+        /* prende i parametri passati al metodo e li inserisce all interno dell attributo aray vuoto */
+        $this->routes[] = [
+            "path" => $path,
+            "params" => $params
+        ];
+    }
+/* metodo match che controlla se all interno dell array corrisponde il path e ne ritorna la variabile con i parametri altrimenti ritorna un valore booleano false */
+    public function match(string $path): array|bool
+    {
+        foreach($this->routes as $route)
+        {
+            if($route["path"] === $path)
+            {
+                return $route["params"];
+            }
+
+        }
+        /* in caso di non match ritorna un valore booleano false */
+        return false;
+    }
+}
